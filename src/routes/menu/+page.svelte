a4d408fe335d4bcb0a9592c8716c0b36de220d3d
CEODemitri <demichole@gmail.com>
2023-10-01 20:08:41 -0500

    working on the menu page

--- src/routes/menu/+page.svelte
+++ src/routes/menu/+page.svelte
@@ -1,1 +1,33 @@
-<h1>Lets Get IT</h1>
+<h1>Menu</h1>
+<a href="/home/ceodemitri/Developer/coding/frameworks/svelte/practice-project/practice-app/src/routes/+page.svelte">Home</a>
+
+<h2>donuts</h2>
+<!-- cards to link to each section -->
+<section>
+    <article>
+        <h3>Sweets</h3>
+        <img src="" alt="">
+    </article>
+    <article>
+        <h3>Treats</h3>
+        <img src="" alt="">
+    </article>
+    <article>
+        <h3>Specials</h3>
+        <img src="" alt="">
+    </article>
+</section>
+
+
+
+<style>
+    section {
+        display: flex;
+        gap: 10px;
+    }
+    article {
+        border: 1px solid white;
+        width: 325px;
+        height: 400px;
+    }
+</style>
